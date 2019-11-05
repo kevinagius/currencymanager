2ddebafca07dcdf249f6a444e01aca1141843e63
kevinagius <38814307+kevinagius@users.noreply.github.com>
2019-11-05 16:48:07 +0100

    Initial Testing
    Testing the tests

--- currencymanager/src/test/java/edu/uom/currencymanager/currencies/CurrencyDatabaseTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/currencies/CurrencyDatabaseTest.java
@@ -1,4 +1,83 @@
 package edu.uom.currencymanager.currencies;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertEquals;
+
 public class CurrencyDatabaseTest {
+
+    CurrencyDatabase currencyDatabase;
+    Currency currency;
+
+
+    @Before
+    public void setup() throws Exception{
+        currencyDatabase = new CurrencyDatabase();
+        currency = new Currency("KVN","KevinBucks",true);
+    }
+
+    @After
+    public void teardown(){
+        currencyDatabase = null;
+        currency = null;
+    }
+
+    @Test
+    public void testGetCurrencyByCode() throws Exception{
+        //Setup
+        currencyDatabase.addCurrency(currency);
+
+        //Exercise
+        currencyDatabase.getCurrencyByCode("KVN");
+
+        //Verify
+        assertEquals("KVN", currencyDatabase.getCurrencyByCode("KVN").code);
+
+        //Teardown
+        currencyDatabase.deleteCurrency("KVN");
+    }
+
+    @Test
+    public void testCurrencyExists() throws Exception{
+        //Setup
+        currencyDatabase.addCurrency(currency);
+
+        //Exercise
+        currencyDatabase.currencyExists("KVN");
+
+        //Verify
+        assertTrue(currencyDatabase.currencyExists("KVN"));
+    }
+
+    @Test
+    public void testAddCurrency() throws Exception{
+        //Exercise
+        currencyDatabase.addCurrency(currency);
+
+        //Verify
+        assertTrue(currencyDatabase.currencyExists("KVN"));
+
+        //Teardown
+        currencyDatabase.deleteCurrency("KVN");
+    }
+
+    @Test
+    public void testDeleteCurrency() throws Exception{
+        //Setup
+        currencyDatabase.addCurrency(currency);
+
+        //Exercise
+        currencyDatabase.deleteCurrency("KVN");
+
+        //Verify
+        assertFalse(currencyDatabase.currencyExists("KVN"));
+    }
+
+
+
+
 }
--- currencymanager/src/test/java/edu/uom/currencymanager/currencies/CurrencyTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/currencies/CurrencyTest.java
@@ -1,33 +1,7 @@
 package edu.uom.currencymanager.currencies;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
-
-import static org.junit.Assert.*;
-
 public class CurrencyTest {
 
-    CurrencyDatabase currencyDatabase;
-    Currency currency;
 
-    @Before
-    public void setup() throws Exception{
-        currencyDatabase = new CurrencyDatabase();
-        currency = new Currency("KVN","KevinBucks",true);
-    }
 
-    @After
-    public void teardown(){
-        currencyDatabase = null;
-        currency = null;
-    }
-
-    @Test
-    public void testFromString(){
-        //Exercise
-
-        //Verify
-    }
 }
