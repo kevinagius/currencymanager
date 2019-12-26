061fa104a90118dd67898b9db09ea8bac0a48b0b
kevinagius <38814307+kevinagius@users.noreply.github.com>
2019-12-26 17:32:30 +0100

    Additions of Lists and Currency

--- currencymanager/src/test/java/edu/uom/currencymanager/currencies/CurrencyDatabaseTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/currencies/CurrencyDatabaseTest.java
@@ -7,17 +7,23 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CurrencyDatabaseTest {
 
     CurrencyDatabase currencyDatabase;
     Currency currency;
 
+    Currency currency0 = new Currency("KVN","KevinBucks",true);
+    Currency currency1 = new Currency("KTH","KeithBucks",true);
 
     @Before
     public void setup() throws Exception{
         currencyDatabase = new CurrencyDatabase();
-        currency = new Currency("KVN","KevinBucks",true);
+        currencyDatabase.addCurrency(currency0);
+        currencyDatabase.addCurrency(currency1);
+
     }
 
     @After
@@ -35,7 +41,7 @@ public class CurrencyDatabaseTest {
     @Test
     public void testGetCurrencyByCode() throws Exception{
         //Setup
-        currencyDatabase.addCurrency(currency);
+        currencyDatabase.addCurrency(currency0);
 
         //Exercise
         currencyDatabase.getCurrencyByCode("KVN");
@@ -47,7 +53,7 @@ public class CurrencyDatabaseTest {
     @Test
     public void testCurrencyExists() throws Exception{
         //Setup
-        currencyDatabase.addCurrency(currency);
+        currencyDatabase.addCurrency(currency0);
 
         //Exercise
         currencyDatabase.currencyExists("KVN");
@@ -58,13 +64,36 @@ public class CurrencyDatabaseTest {
 
     @Test
     public void testGetCurrencies() throws Exception{
+        //Setup
+        List<Currency> currencyDatabaseList = new ArrayList<Currency>();
 
+        List<Currency> currs = new ArrayList<Currency>();
+        currs.add(currency0);
+        currs.add(currency1);
+
+        //Exercise
+        currencyDatabaseList = currencyDatabase.getCurrencies();
+
+        //Verify
+        assertEquals(currs.indexOf(0),currencyDatabaseList.indexOf(0));
+        assertEquals(currs.indexOf(1),currencyDatabaseList.indexOf(1));
     }
 
     @Test
     public void testGetMajorCurrencies() throws Exception{
+        //Setup
+        List<Currency> majorCurrencyDatabaseList = new ArrayList <Currency>();
 
+        List<Currency> majorCurrs = new ArrayList<Currency>();
+        majorCurrs.add(currency0);
+        majorCurrs.add(currency1);
 
+        //Exercise
+        majorCurrencyDatabaseList = currencyDatabase.getMajorCurrencies();
+
+        //Verify
+        assertEquals(majorCurrs.indexOf(0),majorCurrencyDatabaseList.indexOf(0));
+        assertEquals(majorCurrs.indexOf(1),majorCurrencyDatabaseList.indexOf(1));
     }
 
     @Test
