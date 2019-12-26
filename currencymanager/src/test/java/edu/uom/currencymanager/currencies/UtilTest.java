03e40958923f6010655b559d06c2cf6ce02e1baf
kevinagius <38814307+kevinagius@users.noreply.github.com>
2019-12-26 19:49:23 +0100

    Util tests

--- currencymanager/src/test/java/edu/uom/currencymanager/currencies/UtilTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/currencies/UtilTest.java
@@ -1,4 +1,37 @@
 package edu.uom.currencymanager.currencies;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class UtilTest {
+
+    Util util;
+
+    @Before
+    public void setup() throws Exception{
+
+
+    }
+
+    @After
+    public void teardown(){
+        util = null;
+
+    }
+
+    @Test
+    public void testFormatAmount(){
+        //Setup
+        util = new Util();
+
+        //Exercise
+        String decimalFormat = util.formatAmount(123456789.01);
+
+        //Verify
+        assertEquals("123,456,789.01", decimalFormat);
+
+    }
 }
