53069deb40ec1fe84b54c679de5c58353df5c8d2
kevinagius <38814307+kevinagius@users.noreply.github.com>
2020-01-03 16:00:04 +0100

    Test

--- currencymanager/src/test/java/edu/uom/currencymanager/CurrencyManagerTest.java
+++ currencymanager/src/test/java/edu/uom/currencymanager/CurrencyManagerTest.java
@@ -1,5 +1,24 @@
 package edu.uom.currencymanager;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CurrencyManagerTest {
+
+    CurrencyManager currencyManager;
+
+    @Before
+    public void setup() throws Exception{
+
+
+    }
+
+    @After
+    public void teardown(){
+        currencyManager = null;
+    }
+
+
 }
 
